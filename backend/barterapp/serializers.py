bbfd14b85601849d5c112633f048f9c052fd77b8
Monse5634 <monseperez70@live.com>
2025-04-24 14:36:41 -0700

    added role to serializers

--- backend/barterapp/serializers.py
+++ backend/barterapp/serializers.py
@@ -20,7 +20,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'username', 'email', 'password', 'role']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
