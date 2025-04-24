966019ec9fbd52cea0fa58ed46f4fc7a352b2db9
Monse5634 <monseperez70@live.com>
2025-04-24 14:08:03 -0700

    removed role from serializers.py

--- backend/barterapp/serializers.py
+++ backend/barterapp/serializers.py
@@ -20,7 +20,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'role']
+        fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
